f24e0767047c6611db73a583a466e8f13638953c
Allison Perry <allisonpshortnorth@gmail.com>
2020-12-04 18:00:22 -0500

    initial commit

---
+++ lib/candle_lighting.rb
@@ -0,0 +1,2 @@
+class CandleLighting
+end
---
+++ lib/environment.rb
@@ -0,0 +1,7 @@
+require 'pry'
+require 'rest-client'
+require 'json'
+
+require_relative './api'
+require_relative './candle_lighting'
+require_relative './cli'
